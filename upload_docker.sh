f1c3b8ec6c15f5f04373085efba90f429071c0d6
Man Le <manneko1803@gmail.com>
2022-04-23 15:04:52 +0700

    upload docker

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=manlm/api
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
+docker login --username=manlm --email=manneko1803@gmail.com
+docker tag api $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
